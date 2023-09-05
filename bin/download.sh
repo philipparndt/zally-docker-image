c95cdeb15a0e8531cc7c9744c7344fde0ae6d656
Arndt, Philipp <philipp.arndt@vector.com>
2023-09-05 14:22:47 +0200

    publish docker images

--- bin/download.sh
+++ bin/download.sh
@@ -2,7 +2,6 @@
 cd "$(dirname "$0")"
 set -e
 
-export ZALLY_VERSION="v2.1.1"
 URL=https://github.com/zalando/zally/archive/refs/tags/${ZALLY_VERSION}.tar.gz
 
 echo "Downloading Zally ${ZALLY_VERSION} from ${URL}"
